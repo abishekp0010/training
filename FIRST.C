8cf9242f6053696c41f491a82a527bf4035b6da6
abishekp0010 <93249818+abishekp0010@users.noreply.github.com>
2021-10-27 15:05:51 +0530

    Add files via upload

---
+++ FIRST.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int guna=0;
+//****************************main***************************
+void main()
+{
+clrscr();
+	 printf("hello world");
+	 printf("%d",guna);
+getch();
+}
+//***************************END*****************************
